2e3c1b0dedf7532c0c9144705974d562cdc82cc7
Daniel Hernández Fajardo <daniel.hernandez@codebay-innovation.com>
2023-09-15 00:29:12 +0200

    check_condition

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,11 +1,11 @@
 public class ConnectFour {
 	private Grid grid_;
-	private boolean redPlayerTurn_;
 	private boolean gameFinished_;
+	private Turn turn_;
 
 	ConnectFour() {
 		grid_ = new Grid();
-		redPlayerTurn_ = true;
+		turn_ = new Turn();
 		gameFinished_ = false;
 	}
 
@@ -17,67 +17,14 @@ public class ConnectFour {
 
 	public boolean checkwin(String color) {
 
-		if (checkHorizontal(color, i, j)){
+		if (checkHorizontal(color)) {
 			return true;
-		} else if (checkVertical(color, i, j)) 
+		} else if (checkVertical(color)) {
 			return true;
-		} else if (checkDiagonal(color, i, j)) 
+		} else if (checkDiagonal(color)) {
 			return true;
-
-		return false;
-	}
-
-	private boolean checkHorizontal(String color, int row, int col) {
-		int count = 0;
-		for (int j = col; j < col + 4 && j < 7; j++) {
-			if (grid_.getSlot(row, j) == color) {
-				count++;
-				if (count == 4) return true;
-			} else {
-				count = 0;
-			}
 		}
-		return false;
+		return false; 
 	}
 
-	public vertical(color: string): Boolean{
-        for(let i = 0; i < 7; i++){
-            let n: number = 0;
-            for(let j = 0; j < 6; j++){
-                if(this.tablero_[j][i].get_color() == color)
-                    n++;
-                else
-                    n = 0;
-                if(n == 4)
-                    return true;
-            }
-        }
-        return false;
-    }
-
-	private boolean checkVertical(String color, int row, int col) {
-		int count = 0;
-		for (int i = row; i < row + 4 && i < 6; i++) {
-			if (grid_[i][col].get_color() == color) {
-				count++;
-				if (count == 4) return true;
-			} else {
-				count = 0;
-			}
-		}
-		return false;
-	}
-
-	private boolean checkDiagonal(String color, int row, int col) {
-		int count = 0;
-		for (int i = row, j = col; i < row + 4 && i < 6 && j < 7; i++, j++) {
-			if (grid_[i][j].get_color() == color) {
-				count++;
-				if (count == 4) return true;
-			} else {
-				count = 0;
-			}
-		}
-		return false;
-	}
 }
--- Grid.java
+++ Grid.java
@@ -1,27 +1,105 @@
 public class Grid {
-	private Slot[][] gridSlots;
+	private Slot[][] gridSlots_;
 
 	Grid() {
-		gridSlots = new Slot[6][7];
+		gridSlots_ = new Slot[6][7];
 	}
 
 	public String getSlot(int row, int col) {
-		return gridSlots[row][col].get_color();
+		return gridSlots_[row][col].get_color();
 	}
 
 	public boolean setSlot(int col, Slot slot) {
-		if(gridSlots[5][col] != null)
+		if(gridSlots_[5][col] != null)
 			return false;
 
-		gridSlots[checkLastSlot(col) + 1][col] = slot;
+		gridSlots_[checkLastSlot(col) + 1][col] = slot;
 		return true;
 	}
 
 	public int checkLastSlot(int col){
 		for(int i = 0; i < 6; i++){
-			if(gridSlots[i][col] == null)
+			if(gridSlots_[i][col] == null)
 				return i - 1;
 		}
 		return -1;
+	}
+
+
+    public boolean checkHorizontal(String color) {
+		for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 7; j++){
+                tokenCounter (i, j, color);
+            }
+        }
+        return false;
+	}
+	public boolean checkVertical(String color) {
+		for(int i = 0; i < 7; i++){
+            for(int j = 0; j < 6; j++){
+                tokenCounter (j, i, color);
+            }
+        }
+        return false;
+	}
+
+	public boolean checkDiagonal (String color) {
+        
+        if (checkDiagonalsAscendant(color) || checkDiagonalsDescendant(color)) {
+            return true;
+        }
+        return false;
+	}
+
+    public boolean checkDiagonalsAscendant (String color) {
+
+        for(int i = 1; i <= 3; i++){
+            for (int j = 0; j < 7 - i; j++) {
+                if (tokenCounter (j, j + i, color)) {
+                    return true;
+                }
+            }
+        }
+
+        for(int i = 5; i >= 3; i--){
+            for (int j = 0; j <= i; j++) {
+                if (tokenCounter (i - j, j, color)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkDiagonalsDescendant (String color) {
+
+        for(int i = 2; i >= 0; i--){
+            for (int j = 0; j < 6 - i; j++) {
+                if (tokenCounter (i + j, j, color)) {
+                    return true;
+                }
+            }
+        }
+
+        for(int i = 3; i > 0; i--){
+            for (int j = 0; j < 7 - i; j++) {
+				if (tokenCounter (5 - j, j + i, color)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+	public boolean tokenCounter (int row, int col, String color) {
+		int count = 0;
+		if(gridSlots_[row][col].get_color() == color)
+                    count++;
+                else
+                    count = 0;
+                if(count == 4)
+                    return true;
+
+		return false;
 	}
 }
