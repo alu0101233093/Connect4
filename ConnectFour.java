06ae5b9fb2f6371dafd5c2fd6c96fc49f97080f3
alu0101233093 <alu0101233093@ull.edu.es>
2023-09-15 17:54:04 +0200

    game

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,15 +1,51 @@
+import java.util.Scanner;
+
 public class ConnectFour {
 	private Grid grid_;
 	private boolean gameFinished_;
+	private boolean redTurn_;
+	private Scanner input_;
 
 	ConnectFour() {
 		grid_ = new Grid();
 		gameFinished_ = false;
+		input_ = new Scanner(System.in);
+	}
+
+	public int inputToInt() {
+		String input = input_.next();
+		return input.charAt(0) - '0';
+		
 	}
 
 	public void StartGame() {
+		int columnSelected = 0;
 		while(!gameFinished_) {
-
+			if(redTurn_){
+				System.out.println("Red player turn");
+				columnSelected = inputToInt();
+				if(grid_.setSlot(columnSelected, "red")){
+					if(checkwin("red")){
+						System.out.println("Red player wins");
+						gameFinished_ = true;
+					}
+					redTurn_ = false;
+				} else {
+					System.out.println("Column is full");
+				}
+			} else {
+				System.out.println("Yellow player turn");
+				columnSelected = inputToInt();
+				if(grid_.setSlot(columnSelected, "yellow")){
+					if(checkwin("yellow")){
+						System.out.println("Yellow player wins");
+						gameFinished_ = true;
+					}
+					redTurn_ = true;
+				} else {
+					System.out.println("Column is full");
+				}
+			}
 		}
 	}
 
